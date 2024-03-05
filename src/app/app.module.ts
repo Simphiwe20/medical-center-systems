2127e49be906b445ecc86e4fcac9c9940f2cbfb9
Simphiwe Makhathini <gsimphiwe212@gmail.com>
2024-03-05 10:28:26 +0200

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,34 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './components/home/home.component';
+import { LogInComponent } from './components/log-in/log-in.component';
 import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { AddPatientComponent } from './components/Popups/add-patient/add-patient.component';
+import { PrescriptionComponent } from './components/Popups/prescription/prescription.component';
+import { AvailabilityComponent } from './components/Popups/availability/availability.component';
+import { UsersComponent } from './components/users/users.component';
+import { ProfileComponent } from './components/profile/profile.component';
+import { SchedulesComponent } from './components/schedules/schedules.component';
+import { BarGraphComponent } from './components/charts/bar-graph/bar-graph.component';
+import { PieChartsComponent } from './components/charts/pie-charts/pie-charts.component';
+import { ChangePwdComponent } from './components/Popups/change-pwd/change-pwd.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    DashboardComponent
+    HomeComponent,
+    LogInComponent,
+    DashboardComponent,
+    AddPatientComponent,
+    PrescriptionComponent,
+    AvailabilityComponent,
+    UsersComponent,
+    ProfileComponent,
+    SchedulesComponent,
+    BarGraphComponent,
+    PieChartsComponent,
+    ChangePwdComponent,
   ],
   imports: [
     BrowserModule,
