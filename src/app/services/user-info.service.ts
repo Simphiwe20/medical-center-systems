1b4a28ceed0fe817695d956c4b73478d8a5b0e5f
NoxoloMnguni23 <kimberlymnguni@gmail.com>
2024-03-06 09:46:57 +0200

    login page and forgot password

---
+++ src/app/services/user-info.service.ts
@@ -0,0 +1,17 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UserInfoService {
+
+  constructor() { }
+
+  get(key: string, sessionType: string): any {
+    let data = sessionType === 'session' ? sessionStorage.getItem(key) : localStorage.getItem(key);
+    return data ? JSON.parse(data) : data;
+  }
+
+
+  currentUser : any ;
+}
