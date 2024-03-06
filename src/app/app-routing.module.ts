9a48cecfd9a66e265aec4a9f24c1869617de3d61
Simphiwe Makhathini <gsimphiwe212@gmail.com>
2024-03-06 11:17:25 +0200

    changes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,14 @@ import { Component, NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
 import { ForgotPasswordComponent } from './forgot-password/forgot-password.component';
+import { LogInComponent } from './components/log-in/log-in.component';
+import { SchedulesComponent } from './components/schedules/schedules.component';
 
 const routes: Routes = [
-  {path: '', redirectTo: '/home', pathMatch: 'full'},
+  {path: '', redirectTo: '/login', pathMatch: 'full'},
+  {path: 'login', component: LogInComponent},
   {path: 'home', component: HomeComponent},
+  {path: 'schedule', component: SchedulesComponent},
   {path:'forgotPassword',component:ForgotPasswordComponent}
 ];
 
