41095450c4bd25d0b4bf1ef459dc0e7f4bbecf2c
Simphiwe Makhathini <gsimphiwe212@gmail.com>
2024-03-07 09:20:09 +0200

    avail

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,12 +4,14 @@ import { HomeComponent } from './components/home/home.component';
 import { ForgotPasswordComponent } from './forgot-password/forgot-password.component';
 import { LogInComponent } from './components/log-in/log-in.component';
 import { SchedulesComponent } from './components/schedules/schedules.component';
+import { ProfileComponent } from './components/profile/profile.component';
 
 const routes: Routes = [
   {path: '', redirectTo: '/login', pathMatch: 'full'},
   {path: 'login', component: LogInComponent},
   {path: 'home', component: HomeComponent},
   {path: 'schedule', component: SchedulesComponent},
+  {path: 'profile', component: ProfileComponent},
   {path:'forgotPassword',component:ForgotPasswordComponent}
 ];
 
--- src/app/components/Popups/availability/availability.component.ts
+++ src/app/components/Popups/availability/availability.component.ts
@@ -1,4 +1,6 @@
 import { Component, ElementRef, ViewChild } from '@angular/core';
+import { MatDialogRef } from '@angular/material/dialog';
+import { MatSnackBar } from '@angular/material/snack-bar';
 
 @Component({
   selector: 'app-availability',
@@ -9,9 +11,14 @@ import { Component, ElementRef, ViewChild } from '@angular/core';
 export class AvailabilityComponent {
 
   _index: number = 0
+  storeDays: any = localStorage.getItem('days')
+  isTimeSet: boolean = true
 
-  days: any[] = [{day:'Sunday', avail: false, unavail: false, startTime:'', endTime:''}, {day:'Monday', avail: false,  unavail: false, startTime:'', endTime:''}, {day:'Tuesday', avail: false,  unavail: false, startTime:'', endTime:''}, 
-  {day:'Wednesday', avail: false,  unavail: false, startTime:'', endTime:''}, {day:'Tuesday', avail: false,  unavail: false, startTime:'', endTime:''}, {day:'Friday', avail: false,  unavail: false, startTime:'', endTime:''}, {day:'Sartuday', avail: false,  unavail: false, startTime:'', endTime:''}]
+  days: any = !this.storeDays ? [{day:'Sunday', avail: false, unavail: false, startTime:'', endTime:''}, {day:'Monday', avail: false,  unavail: false, startTime:'', endTime:''}, 
+  {day:'Tuesday', avail: false,  unavail: false, startTime:'', endTime:''}, {day:'Wednesday', avail: false,  unavail: false, startTime:'', endTime:''}, {day:'Tuesday', avail: false,  unavail: false, startTime:'', endTime:''}, 
+  {day:'Friday', avail: false,  unavail: false, startTime:'', endTime:''}, {day:'Sartuday', avail: false,  unavail: false, startTime:'', endTime:''}] : JSON.parse(this.storeDays)
+
+  constructor(private dialogRef: MatDialogRef<AvailabilityComponent>, private snackBar: MatSnackBar) {}
 
   updateAvail(indx: number): void {
     if(!this.days[indx].avail && !this.days[indx].unavail)  {
@@ -25,6 +32,21 @@ export class AvailabilityComponent {
       this.days[indx].avail = false
       this.days[indx].unavail = false
     }
+  }
+
+  save(): any {
+    this.days.forEach((day: any, indx: number) => {
+      if(day.avail && (!day.startTime || !day.endTime)) {
+        this.snackBar.open(`You forgot to set time for ${day.day}'s availability`, 'OK', {duration: 3000})
+        this.isTimeSet = false
+      }
+    })
+  if(!this.isTimeSet) return
+   this.close()
+  }
+
+  close() {
+    this.dialogRef.close({data: this.days})
   }
 }
 
