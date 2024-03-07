fd7ba979129e4ba61f18f3d65403d4eb739ea6e4
NoxoloMnguni23 <kimberlymnguni@gmail.com>
2024-03-07 15:31:41 +0200

    update

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,12 +5,13 @@ import { ForgotPasswordComponent } from './forgot-password/forgot-password.compo
 import { LogInComponent } from './components/log-in/log-in.component';
 import { SchedulesComponent } from './components/schedules/schedules.component';
 import { ProfileComponent } from './components/profile/profile.component';
-import { LogInComponent } from './components/log-in/log-in.component';
-import { DashboardComponent } from './components/dashboard/dashboard.component';
 
 const routes: Routes = [
-  {path: '', redirectTo: '/home', pathMatch: 'full'},
+  {path: '', redirectTo: '/login', pathMatch: 'full'},
+  {path: 'login', component: LogInComponent},
   {path: 'home', component: HomeComponent},
+  {path: 'schedule', component: SchedulesComponent},
+  {path: 'profile', component: ProfileComponent},
   {path:'forgotPassword',component:ForgotPasswordComponent}
 ];
 
